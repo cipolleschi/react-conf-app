7d8e0debbf12947a3e76d787bdc3a7277c094101
Kadi Kraman <kadi@expo.io>
2024-06-17 10:54:56 +0100

    Add a react compiler example

---
+++ components/ReactCompilerDemo.tsx
@@ -0,0 +1,79 @@
+import { StyleSheet, Switch } from "react-native";
+import { ThemedText, ThemedView } from "./Themed";
+import { useState } from "react";
+import { theme } from "@/theme";
+import config from "../app.config";
+
+export function ReactCompilerDemo() {
+  const [value, setValue] = useState(false);
+  const isReactCompilerEnabled = config?.expo?.experiments?.reactCompiler;
+
+  return (
+    <>
+      <ThemedText
+        style={styles.centered}
+        marginBottom={theme.space12}
+        fontWeight="bold"
+        fontSize={24}
+      >
+        React Compiler Demo
+      </ThemedText>
+      <ThemedView
+        darkColor="rgba(255,255,255,0.15)"
+        lightColor={theme.lightActiveContent}
+        style={styles.section}
+      >
+        <ThemedText style={{ marginBottom: theme.space8 }}>
+          Toggle the value of this Switch. React compiler is currently{" "}
+          <ThemedText fontWeight="bold">
+            {isReactCompilerEnabled ? "Enabled" : "Disabled"}
+          </ThemedText>{" "}
+          (configured in app.config.js)
+        </ThemedText>
+        <ThemedText style={{ marginBottom: theme.space8 }}>
+          <ThemedText fontWeight="bold">Without React Compiler:</ThemedText> the
+          value in RenderCounter increments with each toggle
+        </ThemedText>
+        <ThemedText style={{ marginBottom: theme.space24 }}>
+          <ThemedText fontWeight="bold">With React Compiler:</ThemedText> the
+          value in RenderCounter does not increment
+        </ThemedText>
+        <ThemedView
+          style={{
+            flexDirection: "row",
+            justifyContent: "space-between",
+            alignItems: "center",
+          }}
+        >
+          <Switch
+            value={value}
+            onValueChange={(value) => setValue(value)}
+            trackColor={{ true: theme.colorReactLightBlue }}
+          />
+          <RenderCounter />
+        </ThemedView>
+      </ThemedView>
+    </>
+  );
+}
+
+let numberOfRenders = 0;
+
+function RenderCounter() {
+  numberOfRenders++;
+  return (
+    <ThemedView style={{ flex: 1, marginLeft: theme.space24 }}>
+      <ThemedText>Number of renders: {numberOfRenders}</ThemedText>
+    </ThemedView>
+  );
+}
+
+const styles = StyleSheet.create({
+  centered: {
+    textAlign: "center",
+  },
+  section: {
+    padding: theme.space24,
+    marginBottom: 200,
+  },
+});
